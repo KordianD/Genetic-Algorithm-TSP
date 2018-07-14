af889c67fe24641a63fa6fa775d81b712a1bdb54
KordianD <kordiandrozd@gmail.com>
2018-07-14 20:54:48 +0200

    removed unnecessary files

--- Path.cpp
+++
@@ -1,9 +0,0 @@
-#include "Path.hpp"
-
-Path::Path(int numberOfPoints, Initializer initializer) : path(numberOfPoints), initializer(initializer)
-{
-    for(auto i = 0; i < numberOfPoints; ++i)
-    {
-        path.emplace_back(initializer.getNumber(), initializer.getNumber());
-    }
-}
